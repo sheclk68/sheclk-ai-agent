98ce48d631878481bc64d689f1f3d28ff232f0bb
sheclk68 <sheclk68@github.com>
2026-05-28 13:53:17 +0000

    Clean up debug/test files, keep only core agent code

--- resolve-test.js
+++
@@ -1,9 +0,0 @@
-const dns = require("dns");
-dns.setServers(["8.8.8.8", "1.1.1.1"]);
-dns.resolve4("quote-api.jup.ag", (err, addresses) => {
-    if (err) {
-        console.log("DNS error:", err.code);
-    } else {
-        console.log("Resolved:", addresses);
-    }
-});
